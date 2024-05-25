e79041fb60eb13c24eff6dc08c8f4e7cc4a487ea
Snagnik <snagnikd@gmail.com>
2024-05-25 06:03:17 +0530

    text implementation

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	id = models.CharField(max_length=5, primary_key=True)
+	name = models.CharField(max_length=30)
+	experience =  models.CharField(max_length=10, default=0)
+	height =  models.CharField(max_length=10)
+	image_url = models.CharField(max_length=200)
+	description = models.CharField(max_length=400)
+
+	def __str__(self):
+		return(f"{self.first_name} {self.last_name}")
